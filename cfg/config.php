c8bd1c805789d033d81734a91acf3f4cfd7c8b7b
Gustavo Seganfredo <gustavosf@gmail.com>
2011-04-29 12:06:14 -0300

    Mudando um pouco a estrutura inicial

--- cfg/config.php
+++ cfg/config.php
@@ -1,3 +1,18 @@
 <?php
 
-define('DUMP_PATH', '../dump');
+define('ROOT', dirname(dirname(__FILE__)));
+define('DS', DIRECTORY_SEPARATOR);
+
+define('DUMP_PATH', ROOT . DS . 'dump');
+define('LIB_PATH', ROOT . DS . 'dump');
+
+/* função mágica usada para carregamento automático dos parsers */
+function __autoload($className) {
+	if (file_exists(LIB_PATH . DS . strtolower($className) . '.parser.php')) {
+		require_once(LIB_PATH . DS . strtolower($className) . '.parser.php');
+	} elseif (file_exists(LIB_PATH . DS . strtolower($className) . '.class.php')) {
+		require_once(LIB_PATH . DS . strtolower($className) . '.class.php');
+	} else {
+		// parser não encontrado :(
+	}
+}
---
+++ parse.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'cfg/config.php';
+require 'lib/parser.class.php';
+
+$parser = new Parser();
+
+?>
